29c5842e711c4f6276dd3607cb4b41cf57d8b572
chesc100 <2289331373@qq.com>
2021-01-21 10:27:45 +0800

    Update setup.py

--- SiddonPythonModule/setup.py
+++ SiddonPythonModule/setup.py
@@ -9,9 +9,9 @@ setup(
     ext_modules = [Extension("SiddonGpuPy",
                              sources=["SiddonGpuPy.pyx"],
                              include_dirs=[numpy.get_include(), 
-                                           "/home/ubuntu/Downloads/CUDA_DigitallyReconstructedRadiographs-master/SiddonPythonModule/include",
+                                           "/home/ubuntu/Downloads/CUDA_DRR/SiddonPythonModule/include",
                                            "/usr/local/cuda/include"],
-                             library_dirs = ["/home/ubuntu/Downloads/CUDA_DigitallyReconstructedRadiographs-master/SiddonPythonModule/lib",
+                             library_dirs = ["/home/ubuntu/Downloads/CUDA_DRR/SiddonPythonModule/lib",
                                              "/usr/local/cuda/lib64"],
                              libraries = ["SiddonGpu", "cudart_static"],
                              language = "c++")]
